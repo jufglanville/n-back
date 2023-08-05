15c5b1ee21ece37a9188a74372e8accb498149da
Joe Glanville <josephglanville@gmail.com>
2023-08-05 15:24:53 +0100

    Update Lives

--- app/game/components/Lives.tsx
+++ app/game/components/Lives.tsx
@@ -3,7 +3,9 @@ interface Props {
 }
 
 export const Lives = ({ lives }: Props) => {
-  const remainingLives = Array.from({ length: lives }).map(() => <p>❤️</p>);
+  const remainingLives = Array.from({ length: lives }).map((el, idx) => (
+    <p key={idx}>❤️</p>
+  ));
 
   return <div className="flex items-center gap-2 mb-5">{remainingLives}</div>;
 };
